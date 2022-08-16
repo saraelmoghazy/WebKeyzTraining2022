8f527c2741e635831757428f6f79b143d65eae3b
Sara Elmoghazy <sara.elmoghazy@yahoo.com>
2022-08-16 17:57:32 +0100

    change image link

--- app/src/main/java/com/example/training/MainActivity.kt
+++ app/src/main/java/com/example/training/MainActivity.kt
@@ -28,17 +28,17 @@ class MainActivity : AppCompatActivity() {
             Employee(
                 "shimaa",
                 "trainee",
-                "https://cdn.shrm.org/image/upload/c_crop%2Ch_3888%2Cw_6917%2Cx_0%2Cy_253/c_fit%2Cf_auto%2Cq_auto%2Cw_767/v1/Employee%20Relations/bored_worker_photo_k9vufj?databtoa=eyIxNng5Ijp7IngiOjAsInkiOjI1MywieDIiOjY5MTcsInkyIjo0MTQxLCJ3Ijo2OTE3LCJoIjozODg4fSwiMXgxIjp7IngiOjEzMDQsInkiOjAsIngyIjo1NjY2LCJ5MiI6NDM1NywidyI6NDM2MywiaCI6NDM1N319"
+                "https://cdn.xxl.thumbs.canstockphoto.com/happy-employee-portrait-of-successful-businesswoman-looking-at-camera-with-smile-picture_csp7091881.jpg"
             ),
             Employee(
                 "sara",
                 "senior android",
-                "https://media.istockphoto.com/photos/portrait-of-cheerful-businesswoman-smiling-at-the-meeting-picture-id1218610083?k=20&m=1218610083&s=612x612&w=0&h=OwaDQ2DQu_7E9cz0n1rdXUFPckoKNpWsJU92pPPiRok="
+                "https://cdn.xxl.thumbs.canstockphoto.com/happy-employee-portrait-of-successful-businesswoman-looking-at-camera-with-smile-picture_csp7091881.jpg"
             ),
             Employee(
                 "Yahia",
                 "web developer",
-                "https://images.pexels.com/photos/927022/pexels-photo-927022.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                "https://cdn.xxl.thumbs.canstockphoto.com/happy-employee-portrait-of-successful-businesswoman-looking-at-camera-with-smile-picture_csp7091881.jpg"
             )
         )
 
